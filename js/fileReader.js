dd9f4f00bcfbcdd08765d2a637c538a5d405b5a9
chenjunshen <022927@sichuanair.com>
2021-11-23 17:27:04 +0800

    逻辑提交

--- js/fileReader.js
+++ js/fileReader.js
@@ -1,4 +1,9 @@
-function read(input,callback) {
+/**
+ * 读取本地文件
+ * @param input
+ * @param callback
+ */
+function read(input, callback) {
     //支持chrome IE10
     if (window.FileReader) {
         var file = input.files[0];
@@ -6,7 +11,7 @@ function read(input,callback) {
         var reader = new FileReader();
         reader.onload = function () {
             console.log(this.result);
-            if(typeof callback === 'function'){
+            if (typeof callback === 'function') {
                 callback(toJSON(this.result));
             }
         }
@@ -19,7 +24,7 @@ function read(input,callback) {
         xmlDoc.async = false;
         xmlDoc.load(input.value);
         console.log(xmlDoc.xml);
-        if(typeof callback === 'function'){
+        if (typeof callback === 'function') {
             callback(toJSON(xmlDoc.xml));
         }
     }
@@ -30,7 +35,7 @@ function read(input,callback) {
         xmlDoc.async = false;
         xmlDoc.load(input.value);
         console.log(xmlDoc.xml);
-        if(typeof callback === 'function'){
+        if (typeof callback === 'function') {
             callback(toJSON(xmlDoc.xml));
         }
     } else {
@@ -38,6 +43,11 @@ function read(input,callback) {
     }
 }
 
+/**
+ * 普通String对象转换为JSON对象
+ * @param result
+ * @returns {null}
+ */
 function toJSON(result) {
     if (result) {
         var obj = null;
@@ -51,5 +61,38 @@ function toJSON(result) {
         return obj;
     } else {
         return null;
+    }
+}
+
+function isArrayFn(value) {
+    if (typeof Array.isArray === "function") {
+        return Array.isArray(value);
+    } else {
+        return Object.prototype.toString.call(value) === "[object Array]";
+    }
+}
+
+function isEmpty(s) {
+    return s === null || s === '' || s === undefined;
+}
+var count=-1;
+var tempArray=[];
+function sbScriptParser(json) {
+    if (json != null) {
+        for (var p in json) {
+            if (isArrayFn(p)) {//数组
+                sbScriptParser(p)
+            } else {//普通对象
+                for(var pp in p){
+                    if (!isEmpty(pp['Eng']) && isEmpty(pp['Chs'])) {
+                        count++;
+                        p['Chs']=count+'';
+                        tempArray.push(p['Eng']);
+                    }else{
+                        sbScriptParser(pp);
+                    }
+                }
+            }
+        }
     }
 }
